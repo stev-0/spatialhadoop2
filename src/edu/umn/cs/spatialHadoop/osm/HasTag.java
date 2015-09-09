cda6c173779ee3512880577e2ac7bda2a911ed35
Stephen Knox <stephenknox73@gmail.com>
2015-09-09 20:03:07 +0000

    add check for nulls when checking OSM entities for specific tags

--- src/edu/umn/cs/spatialHadoop/osm/HasTag.java
+++ src/edu/umn/cs/spatialHadoop/osm/HasTag.java
@@ -37,12 +37,19 @@ public class HasTag extends EvalFunc<Boolean> {
     Map<String, String> tags = (Map<String, String>) input.get(0);
     String keys = (String)input.get(1);
     String values = (String)input.get(2);
-
+    // check that item has tags (although this should have been filtered out)
+    if (tags == null) {
+      return false;
+    }
     return hasTag(tags, keys, values);
   }
 
   public static boolean hasTag(Map<String, String> tags, String keys, String values) {
     for (Map.Entry<String, String> entry : tags.entrySet())
+      // avoid null pointer exception if tag has no value e.g. shop=''
+      if entry.getValue() == null) {
+        return false;
+      }
       if (keys.contains(entry.getKey()) && values.contains(entry.getValue()))
         return true;
     return false;
